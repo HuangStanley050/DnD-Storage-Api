737dce04b888eeb51cc3243b75a96f324500ba4d
Stanley Huang <huang.stanley050@gmail.com>
2019-09-08 17:45:33 +1000

    able to upload multiple files to google storage

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 import logger from "morgan";
 import * as admin from "firebase-admin";
 import serviceAccount from "./pwagram-bd625-firebase-adminsdk-ew3ye-db17bc2116.json";
@@ -10,11 +10,11 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "gs://pwagram-bd625.appspot.com/"
 });
-
-const app = express();
 const auth = admin.auth();
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
+
+const app = express();
 
 app.set("auth", auth);
 app.set("db", db);
@@ -23,7 +23,7 @@ app.set("bucket", bucket);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/data", dataRouter);
 
--- src/controllers/data.js
+++ src/controllers/data.js
@@ -1,6 +1,25 @@
 export default {
-  storeFiles: async (req, res, next) => {
-    console.log(req.files);
-    res.json({ msg: "Storing file controller" });
+  storeFiles: (req, res, next) => {
+    const bucket = req.app.get("bucket");
+
+    req.files.forEach(async (file, i) => {
+      let tempblob = bucket.file(file.originalname);
+
+      let tempblob_stream = tempblob.createWriteStream();
+
+      tempblob_stream.on("error", err => {
+        next(err);
+      });
+
+      tempblob_stream.on("finish", () => {
+        // The public URL can be used to directly access the file via HTTP.
+        //const publicUrl = `https://storage.googleapis.com/${bucket.name}/${tempblob.name}`;
+        //res.status(200).send(publicUrl);
+        if (i === req.files.length) {
+          res.json({ msg: "Upload files success" });
+        }
+      });
+      tempblob_stream.end(req.files[i].buffer);
+    });
   }
 };
