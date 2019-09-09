c64752d0f2536786001a1afe00986b0c1b926650
Stanley Huang <huang.stanley050@gmail.com>
2019-09-09 20:36:03 +1000

    confirm able to add uploaded data to firestore

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -2,8 +2,9 @@ import uuid from "uuid/v4";
 export default {
   storeFiles: async (req, res, next) => {
     const bucket = req.app.get("bucket");
+    const db = req.app.get("db");
     let upload_queue = [];
-
+    let database_queue = [];
     let fileSize;
     let fileType;
     let creationTime;
@@ -11,17 +12,6 @@ export default {
     let fileInfo;
 
     req.files.forEach((file, index) => {
-      fileSize = file.size;
-      fileType = file.mimetype;
-      creationTime = Date.now();
-      fileInfo = {
-        fileType,
-        fileSize,
-        creationTime,
-        creator,
-        name: file.originalname
-      };
-
       let tempblob = bucket.file(`DnD-${uuid()}-${file.originalname}`);
       const newPromise = new Promise((resolve, reject) => {
         tempblob
@@ -36,6 +26,22 @@ export default {
       });
       upload_queue.push(newPromise);
     });
+
+    req.files.forEach(file => {
+      fileSize = file.size;
+      fileType = file.mimetype;
+      creationTime = Date.now();
+      fileInfo = {
+        fileType,
+        fileSize,
+        creationTime,
+        creator,
+        name: file.originalname
+      };
+      let database_result = db.collection("UploadedFiles").add(fileInfo);
+      database_queue.push(database_result);
+    });
+
     try {
       await Promise.all(upload_queue);
     } catch (err) {
@@ -43,5 +49,12 @@ export default {
     }
 
     res.json({ msg: "upload successful" });
+
+    try {
+      let result = await Promise.all(database_queue);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   }
 };
